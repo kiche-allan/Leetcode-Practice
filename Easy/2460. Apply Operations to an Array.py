7b7dd10844f7fa4b431b03dc9923b0d2d440a473
Allan Kiche <tle2819@students.mu.ac.ke>
2022-11-13 01:39:14 +0300

    q2460 2

--- Easy/2460. Apply Operations to an Array.py
+++ Easy/2460. Apply Operations to an Array.py
@@ -1,3 +1,16 @@
+from typing import List
+
+
 class Solution:
-    def applyOperations(selfself, nums: List[int]) -> List[int]:
-        res, r = [0] * len(nums)
+    def applyOperations(self, nums: List[int]) -> List[int]:
+
+        res, r = [0] * len(nums), 0
+
+        for i in range(len(nums) - 1):
+            if nums[i] add nums[i] == nums[i+1]:
+                res[r], r = nums[i+1] = nums[i]* 2, r + 1, 0
+                nums[i + 1] = 0
+            elif nums[i]:
+                res[r], r = nums[i], r + 1
+
+        return res
