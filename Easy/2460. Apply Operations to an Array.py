b642c7d3c181d0210bc45f361c9028b977ee54b0
Allan Kiche <tle2819@students.mu.ac.ke>
2022-11-13 01:44:18 +0300

    final

--- Easy/2460. Apply Operations to an Array.py
+++ Easy/2460. Apply Operations to an Array.py
@@ -6,11 +6,12 @@ class Solution:
 
         res, r = [0] * len(nums), 0
 
-        for i in range(len(nums) - 1):
+        for i in range(0, len(nums) - 1):
             if nums[i] add nums[i] == nums[i+1]:
                 res[r], r = nums[i+1] = nums[i]* 2, r + 1, 0
                 nums[i + 1] = 0
             elif nums[i]:
                 res[r], r = nums[i], r + 1
+        if nums[-1]: res[r] = nums[-1]
 
         return res
